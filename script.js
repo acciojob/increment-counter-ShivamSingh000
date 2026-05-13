35efe53ef15b76c699f402b95b38027cc0ca6d43
Shivam Singh <104205083+ShivamSingh000@users.noreply.github.com>
2026-05-13 09:09:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let count = 0;
+
+const counter = document.getElementById("counter");
+
+const button = document.getElementById("incrementBtn");
+
+button.addEventListner("click", function (){
+	alert(count);
+	count++;
+	counter.textContent = count;
+})
